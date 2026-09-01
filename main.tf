48fc75fc4737167c2b4a5c90374be53377a8d512
Kapish0879 <kapishvibhute@gmail.com>
2026-09-01 16:30:09 +0530

    update files

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
 provider "aws" {
     region = "us-east-1"
-    key_name = "kapish"
+    
   
 }
 
 resource "aws_instance" "ec2" {
     ami           = "var.ami"
     instance_type = "var.instance_type"
+    key_name = "kapish"
   
     tags = {
       Name = "usingtf"
--- var.tf
+++ var.tf
@@ -1,8 +1,6 @@
 variable "ami" {
     default = "ami-081b0a6eac00b4f53"
-  
 }
 variable "instance_type" {
     default = "t2.micro"
-  
 }
