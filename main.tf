7509c638459f031c0b8d3ae359e1da8dfb948561
Kapish0879 <kapishvibhute@gmail.com>
2026-09-02 12:41:14 +0530

    add and update files

--- main.tf
+++ main.tf
@@ -1,15 +1,51 @@
-provider "aws" {
-    region = "us-east-1"
-    profile = "kapish"
-    
-  
+data "aws_vpc" "default" {
+    default = true
 }
 
+
+ resource "aws_security_group" "sg" {
+     name   = "my-security-group"
+     vpc_id = data.aws_vpc.default.id
+
+     ingress {
+         from_port   = 22
+         to_port     = 22
+         protocol    = "tcp"
+         cidr_blocks = ["0.0.0.0/0"]
+     }
+     ingress = {
+        
+         from_port   = 80
+         to_port     = 80
+         protocol    = "tcp"
+         cidr_blocks = ["0.0.0.0/0"]
+     }
+     
+    egress = {
+        
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    tags = {
+        Name = "my-security"
+    }
+ }
+
+
+ 
 resource "aws_instance" "ec2" {
     ami           = var.ami
     instance_type = var.instance_type
-    key_name = "kapish"
-  
+    key_name = var.key_name
+    vpc_security_group_ids = [aws_security_group.sg.id]
+    user_data = file("/home/ubuntu/Terraform/userdata.sh")
+
+root_block_device {
+  volume_size = var.volume_size
+  volume_type = var.volume_type
+}
     tags = {
       Name = "usingtf"
     }
--- var.tf
+++ var.tf
@@ -1,6 +1,20 @@
 variable "ami" {
     default = "ami-081b0a6eac00b4f53"
 }
+
 variable "instance_type" {
     default = "t2.micro"
 }
+
+variable "volume_size" {
+    default = 8
+}
+
+variable "volume_type" {
+    default = "gp3"
+}
+
+variable "key_name" {
+    default = "Kapish-med-erp"
+}
+
