5acd7a61d6e3054da5dfca863f8a75e98bbd381e
Kapish0879 <kapishvibhute@gmail.com>
2026-09-05 14:31:13 +0530

    user data.sh: Update index.html path to /var/www/html

--- day_3/user_data.sh
+++ day_3/user_data.sh
@@ -3,6 +3,6 @@ apt update -y
 apt install nginx -y
 systemctl start nginx
 systemctl enable nginx
-rm -rf /usr/share/nginx/html/*
+rm -rf /var/www/html/*
 echo "<h1> Hello From kapish </h1>" > /var/www/html/index.html
 systemctl restart nginx
