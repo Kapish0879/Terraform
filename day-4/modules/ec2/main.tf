abb17ecaf75b59455964d37515137b5ed042a7a2
Kapish0879 <kapishvibhute@gmail.com>
2026-09-05 16:58:38 +0530

    ecreate security group and associate it with EC2 instance

--- day-4/modules/ec2/main.tf
+++ day-4/modules/ec2/main.tf
@@ -1,9 +1,9 @@
-resource "aws_instance" "name" {
+resource "aws_instance" "public_instance" {
   ami           = var.ami
   instance_type = var.instance_type
   key_name      = var.key_name
   vpc_security_group_ids = [var.sg_id]
-  subnet_id = var.subnet_id
+  subnet_id = var.public_subnet_id
   user_data = file("/home/ubuntu/Terraform/day-4/modules/ec2/user_data.sh")
   tags = {
     Name = "my-ec2-public-instance"
