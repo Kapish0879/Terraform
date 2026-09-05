c68f5a74d3cb14ff1bb601149562a36fbf1401d0
Kapish0879 <kapishvibhute@gmail.com>
2026-09-05 14:27:33 +0530

    user data updated to point to the correct nginx html directory

--- day_3/user_data.sh
+++ day_3/user_data.sh
@@ -4,5 +4,5 @@ apt install nginx -y
 systemctl start nginx
 systemctl enable nginx
 rm -rf /usr/share/nginx/html/*
-echo "<h1> Hello From kapish </h1>" > /usr/share/nginx/html/index.html
+echo "<h1> Hello From kapish </h1>" > /var/www/html/index.html
 systemctl restart nginx
