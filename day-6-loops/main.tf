389101442653e730fe4496c27f12dbc6b071c0f8
Kapish0879 <kapishvibhute@gmail.com>
2026-09-10 15:34:06 +0530

    add day-6-loops files for aws instance creation with loops

---
+++ day-6-loops/main.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "ec2" {
+    for_each = tomap({
+        server1 = "t3.micro"
+        server2 = "c7i.flex.large"
+        server3 = "t3.small"
+    })
+    ami           = "ami-0b6d9d3d33ba97d99"
+    instance_type = each.value
+    key_name      = "Kapish-med-erp"
+    tags = {
+        Name = "EC2Instance"
+    }
+  
+}
