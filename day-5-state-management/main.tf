9928b11857a54a61070647c4db0ac51ddf475efc
Kapish0879 <kapishvibhute@gmail.com>
2026-09-10 15:17:50 +0530

    edit key name and update S3 bucket for state management

--- day-5-state-management/main.tf
+++ day-5-state-management/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "public-instance" {
   ami           = "ami-0b6d9d3d33ba97d99"
   instance_type = "t3.micro"
-  key_name      = "kapish-med-erp"
+  key_name      = "Kapish-med-erp"
   vpc_security_group_ids = ["sg-01ec9cb850e52eb3f"]
   count = 2
   tags = {
